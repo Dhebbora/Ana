f2edbe5a822ae7831260af9d5731390a5c2044f1
Dhebbora <45688600+Dhebbora@users.noreply.github.com>
2023-11-22 10:54:41 -0300

    Add files via upload

---
+++ ikobe/animais_aquaticos.js
@@ -0,0 +1,9 @@
+function showModal() {
+    document.getElementById("overlay").style.display = "flex";
+    document.getElementById("card-animal-open").style.display = "block";
+}
+
+function hideModal() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("card-animal-open").style.display = "none";
+}
